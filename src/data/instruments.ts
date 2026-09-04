bde6bdff73d6502effe81badd775f3af3973aef5
Quentin Lintz <quentinlintz@outlook.com>
2026-09-03 21:43:07 -0400

    chore: configure prettier for astro

--- src/data/instruments.ts
+++ src/data/instruments.ts
@@ -1,27 +1,30 @@
 export interface InstrumentGuideEntry {
   label: string;
-  descriptions: string[]
+  descriptions: string[];
 }
 
 export const instruments: InstrumentGuideEntry[] = [
   {
-    label: 'Chord-capable',
-    descriptions: ["Any chord voicing of G"]
+    label: "Chord-capable",
+    descriptions: ["Any chord voicing of G"],
   },
   {
-    label: 'Single-note',
-    descriptions: ["Either concert or written G"]
+    label: "Single-note",
+    descriptions: ["Either concert or written G"],
   },
   {
-    label: 'Voice',
-    descriptions: ["Sing the pitch G in any comfortable octave"]
+    label: "Voice",
+    descriptions: ["Sing the pitch G in any comfortable octave"],
   },
   {
-    label: 'Percussion',
-    descriptions: ["A flam, crash, or your own interpretation"]
+    label: "Percussion",
+    descriptions: ["A flam, crash, or your own interpretation"],
   },
   {
-    label: 'Electronic',
-    descriptions: ["Play or trigger a G using your synth, sampler, sequencer, or modular system", "A sound or sequence without a particular pitch is welcome, too"]
+    label: "Electronic",
+    descriptions: [
+      "Play or trigger a G using your synth, sampler, sequencer, or modular system",
+      "A sound or sequence without a particular pitch is welcome, too",
+    ],
   },
-]
+];
