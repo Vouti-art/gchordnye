d7e25fbd8dc2f952bdc7b74ecabc008ba732f348
Quentin Lintz <quentinlintz@outlook.com>
2026-09-04 12:56:45 -0400

    chore: configure custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://quentinlintz.github.io",
-  base: "/gchordnye",
+  site: "https://gchordnye.com",
 });
